e142245f933effc45dde018606ed9412c9de068e
CrstianoFranca1976 <cristiano.franca@hotmail.co.uk>
2025-04-13 10:42:50 +0100

    add  link repository

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
-  res.send("Teste usando Vercel com Node.js")
+  res.send("Teste usando Vercel com Node.js\n For more information you can visit my repository: https://github.com/CristianoFranca1976/vercel_node")
 });
 
 app.listen(port, () => {
